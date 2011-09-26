da28b5df2ce58148eb493ecb84a39c7901c1a671
Xavier Ordoquy <xordoquy@linovia.com>
2011-09-26 09:34:50 +0200

    Do not import django generic views but use my owns.

--- ecbv/__init__.py
+++ ecbv/__init__.py
@@ -1,10 +1,10 @@
-from django.views.generic.base import View, TemplateView, RedirectView
-from django.views.generic.dates import (ArchiveIndexView, YearArchiveView, MonthArchiveView,
+from .base import View, TemplateView, RedirectView
+from .dates import (ArchiveIndexView, YearArchiveView, MonthArchiveView,
                                      WeekArchiveView, DayArchiveView, TodayArchiveView,
                                      DateDetailView)
-from django.views.generic.detail import DetailView
-from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
-from django.views.generic.list import ListView
+from .detail import DetailView
+from .edit import FormView, CreateView, UpdateView, DeleteView
+from .list import ListView
 
 import detail, edit, list, base
 
